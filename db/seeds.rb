3d42f65c7da3ae3c5c9fcb75253d7eb9f2ce5e29
dangerstephen <dangersteve1@gmail.com>
2016-12-19 15:53:01 -0800

    inital commit, set up basic auth using the gem 'devise'

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+User.create([
+  {email: "ross@example.com", password: "password"},
+  {email: "rachel@example.com", password: "password"},
+  {email: "chandler@example.com", password: "password"},
+  {email: "joey@example.com", password: "password"},
+  {email: "monica@example.com", password: "password"},
+  {email: "phoebe@example.com", password: "password"}
+])
